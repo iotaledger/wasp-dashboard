71bd4a0bf729d32a7fd87d09ba9614267495ea43
Marc Espín <mespinsanz@gmail.com>
2022-12-04 19:11:13 +0100

    refactor: move from `tanglesService` to `waspClientService` (#55)
    * feat: Refactor
    
    * fix: Cleaned up code
    
    * fix: Typo
    
    * feat: Update the wasp client on it's own
    
    * fix: Cleaned up code

--- src/app/routes/HomeState.ts
+++ src/app/routes/HomeState.ts
@@ -1,4 +1,3 @@
-
 export interface HomeState {
     /**
      * The version.
--- src/services/waspClientService.ts
+++ src/services/waspClientService.ts
@@ -1,11 +1,14 @@
 import { Environment } from "../environment";
+import { ServiceFactory } from "../factories/serviceFactory";
+import { EventAggregator } from "./eventAggregator";
+import { LocalStorageService } from "./localStorageService";
 import { ChainsApi, Configuration, NodeApi, RequestsApi, UsersApi } from "./wasp_client";
 
 /**
  * Class to manage the wasp API.
  */
 export class WaspClientService {
-    private readonly _apiClients: {
+    private _apiClients?: {
         users: UsersApi;
         node: NodeApi;
         chains: ChainsApi;
@@ -13,32 +16,43 @@ export class WaspClientService {
     };
 
     constructor() {
+        this.initialize();
+
+        EventAggregator.subscribe("auth-state", "waspClient", (isLoggedIn) => {
+            if (isLoggedIn) {
+                this.initialize();
+            }
+        });
+    }
+
+    public initialize() {
+        const storageService = ServiceFactory.get<LocalStorageService>("local-storage");
         const config: Configuration = new Configuration({
-            // Add JWT Token here at some point (from authService)
-            basePath: Environment.WaspApiUrl
+            apiKey: storageService.load("dashboard-jwt"),
+            basePath: Environment.WaspApiUrl,
         });
 
         this._apiClients = {
             users: new UsersApi(config),
             chains: new ChainsApi(config),
             node: new NodeApi(config),
-            requests: new RequestsApi(config)
+            requests: new RequestsApi(config),
         };
     }
 
     public users(): UsersApi {
-        return this._apiClients.users;
+        return this._apiClients?.users as UsersApi;
     }
 
     public node(): NodeApi {
-        return this._apiClients.node;
+        return this._apiClients?.node as NodeApi;
     }
 
     public chains(): ChainsApi {
-        return this._apiClients.chains;
+        return this._apiClients?.chains as ChainsApi;
     }
 
     public requests(): RequestsApi {
-        return this._apiClients.requests;
+        return this._apiClients?.requests as RequestsApi;
     }
 }
