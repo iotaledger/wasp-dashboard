2fe1f11d65d450f95a4ce9f2a993aeacc577ed0c
marc2332 <mespinsanz@gmail.com>
2022-12-13 11:10:28 +0100

    fix: Log out when JWT is expired or malformed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -144,6 +144,10 @@ class App extends AsyncComponent<RouteComponentProps, AppState> {
             // Raise exception message to frontend
         }
 
+        if (this.state.isLoggedIn) {
+            this.validateTokenPeriodically();
+        }
+
         EventAggregator.subscribe("auth-state", "app", (isLoggedIn) => {
             this.setState(
                 {
@@ -389,21 +393,26 @@ class App extends AsyncComponent<RouteComponentProps, AppState> {
      * Refresh the token one minute before it expires.
      */
     private validateTokenPeriodically() {
-        this.clearTokenExpiryInterval();
-        const jwt = this._storageService.load<string>("dashboard-jwt");
-        const expiryTimestamp = this.getTokenExpiry(jwt);
-        const expiryDate = moment(expiryTimestamp);
-        const refreshTokenDate = moment(expiryDate).subtract(1, "minutes");
+        try {
+            this.clearTokenExpiryInterval();
+            const jwt = this._storageService.load<string>("dashboard-jwt");
+            const expiryTimestamp = this.getTokenExpiry(jwt);
+            const expiryDate = moment(expiryTimestamp);
+            const refreshTokenDate = moment(expiryDate).subtract(1, "minutes");
 
-        this._tokenExpiryTimer = setInterval(async () => {
-            const now = moment();
-            if (now.isAfter(expiryDate)) {
-                this._authService.logout();
-                this.clearTokenExpiryInterval();
-            } else if (now.isBetween(refreshTokenDate, expiryDate)) {
-                await this._authService.initialize();
-            }
-        }, 5000);
+            this._tokenExpiryTimer = setInterval(async () => {
+                const now = moment();
+                if (now.isAfter(expiryDate)) {
+                    this._authService.logout();
+                    this.clearTokenExpiryInterval();
+                } else if (now.isBetween(refreshTokenDate, expiryDate)) {
+                    await this._authService.initialize();
+                }
+            }, 5000);
+        } catch {
+            this._authService.logout();
+            this.clearTokenExpiryInterval();
+        }
     }
 
     /**
