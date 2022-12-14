77ca527b0a7c402a589433ba678e9c5e7b0eb91d
marc2332 <mespinsanz@gmail.com>
2022-12-14 12:51:29 +0100

    feat: Show the aggregated messages from the node in the l1 view

--- src/app/routes/L1.tsx
+++ src/app/routes/L1.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as HealthGoodIcon } from "../../assets/health-good.svg";
 import { ServiceFactory } from "../../factories/serviceFactory";
 import "./L1.scss";
 import { NodeConfigService } from "../../services/nodeConfigService";
-import { ChainInfoResponse, L1Params } from "../../services/wasp_client";
+import { ChainInfoResponse, ChainMetrics, L1Params } from "../../services/wasp_client";
 import { WaspClientService } from "../../services/waspClientService";
 
 /**
@@ -14,6 +14,7 @@ import { WaspClientService } from "../../services/waspClientService";
 function L1() {
     const [l1Params, setL1Params] = useState<L1Params | null>(null);
     const [chains, setChains] = useState<ChainInfoResponse[] | null>(null);
+    const [l1Metrics, setl1Metrics] = useState<ChainMetrics | null>(null);
 
     React.useEffect(() => {
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -36,50 +37,105 @@ function L1() {
                 .then((newChains) => {
                     setChains(newChains);
                 });
+
+            // eslint-disable-next-line @typescript-eslint/no-floating-promises
+            waspClientService
+                .metrics()
+                .getL1Metrics()
+                .then((metrics) => {
+                    setl1Metrics(metrics);
+                });
         })();
     }, []);
 
     return (
         <div className="l1">
             <div className="content">
-                <div className="card col fill">
-                    <div className="l1-summary">
-                        <h4>L1 params</h4>
-                        {l1Params &&
-                            Object.entries(l1Params).map(([key, val]: [string, Record<string, string>]) => {
-                                const isObject = typeof val === "object";
-                                return (
-                                    <div key={key} className="l1-info--item">
-                                        <h2>{key}</h2>
-                                        {isObject ? (
-                                            <ul>
-                                                {Object.entries(val).map(([prop, propVal]) => (
-                                                    <li key={prop}>
-                                                        <b>{prop}:</b> {`${propVal}`}
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        ) : (
-                                            <p>{val}</p>
-                                        )}
-                                    </div>
-                                );
-                            })}
-                    </div>
-                </div>
-                <div className="card col">
-                    <div className="l1-summary">
-                        <h4>Chains</h4>
-                        {chains?.map((chain) => (
-                            <div key={chain.chainID} className="l1-summary--item">
-                                <div className="l1-health-icon">
-                                    {chain.isActive ? <HealthGoodIcon /> : <HealthBadIcon />}
-                                </div>
-                                <p className="l1-id" title={chain.chainID}>
-                                    {addressShorter(chain.chainID)}
-                                </p>
+                <div className="group column">
+                    <div className="group">
+                        <div className="card col fill">
+                            <div className="l1-summary">
+                                <h4>L1 params</h4>
+                                {l1Params &&
+                                    Object.entries(l1Params).map(([key, val]: [string, Record<string, string>]) => {
+                                        const isObject = typeof val === "object";
+                                        return (
+                                            <div key={key} className="l1-info--item">
+                                                <h2>{key}</h2>
+                                                {isObject ? (
+                                                    <ul>
+                                                        {Object.entries(val).map(([prop, propVal]) => (
+                                                            <li key={prop}>
+                                                                <b>{prop}:</b> {`${propVal}`}
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                ) : (
+                                                    <p>{val}</p>
+                                                )}
+                                            </div>
+                                        );
+                                    })}
                             </div>
-                        ))}
+                        </div>
+                        <div className="card col">
+                            <div className="l1-summary">
+                                <h4>Chains</h4>
+                                {chains?.map((chain) => (
+                                    <div key={chain.chainID} className="l1-summary--item">
+                                        <div className="l1-health-icon">
+                                            {chain.isActive ? <HealthGoodIcon /> : <HealthBadIcon />}
+                                        </div>
+                                        <p className="l1-id" title={chain.chainID}>
+                                            {addressShorter(chain.chainID)}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card col fill">
+                        <div className="l1-summary">
+                            <h4>L1 metrics</h4>
+                            {l1Metrics && (
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Message name</th>
+                                            <th>Type</th>
+                                            <th>Total</th>
+                                            <th>Last time</th>
+                                            <th>Last message</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {Object.entries(l1Metrics).map(([key, val]: [string, StandardMessage]) => {
+                                            const name = METRICS_NAMES[key];
+                                            const inOrOut = key.startsWith("in") ? "IN" : "OUT";
+                                            const totalMessages = val.messages ?? 0;
+                                            const date =
+                                                val.timestamp.valueOf() > 0 ? val.timestamp.toISOString() : "NEVER";
+                                            const lastMessage = val.lastMessage
+                                                ? `${JSON.stringify(val.lastMessage).slice(0, 45)}...`
+                                                : "";
+                                            return (
+                                                <tr key={key}>
+                                                    <td>{name}</td>
+                                                    <td>{inOrOut}</td>
+                                                    <td>{totalMessages}</td>
+                                                    <td>{date}</td>
+                                                    <td>
+                                                        <pre>
+                                                            <code>{lastMessage}</code>
+                                                        </pre>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </table>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -87,6 +143,29 @@ function L1() {
     );
 }
 
+interface StandardMessage {
+    messages?: number;
+    timestamp: Date;
+    lastMessage?: {
+        output: {
+            bytes: string;
+        };
+        outputID: string;
+    };
+}
+
 const addressShorter = (addr?: string) => `${addr?.slice(0, 30)}...`;
+
+const METRICS_NAMES: Record<string, string> = {
+    inAliasOutput: "Alias output",
+    inOnLedgerRequest: "On ledger request",
+    inOutput: "Output",
+    inStateOutput: "State output",
+    inTxInclusionState: "InTxInclusionState",
+    outPublishGovernanceTransaction: "Publish governance tx",
+    outPullLatestOutput: "Pull latest output",
+    outPullOutputByID: "Pull output by ID",
+    outPullTxInclusionState: "Pull tx inclusion state",
+};
 
 export default L1;
