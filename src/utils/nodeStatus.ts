7badcbba5b7e8a66e6316ef6e7bebbebfdf846c5
Marc Espín <mespinsanz@gmail.com>
2022-12-14 10:36:01 +0100

    feat: improve how the api status is checked (#98)

--- src/utils/nodeStatus.ts
+++ src/utils/nodeStatus.ts
@@ -1,15 +1,12 @@
 import { Environment } from "../environment";
 
-const EXPECTED_RESPONSE = { Error: "code=404, message=Not Found" };
-
 /**
  * Check if the Node API is up and running
  */
 export default async function isNodeOnline(): Promise<boolean> {
     try {
-        const res = await fetch(Environment.WaspApiUrl);
-        const data = await res.json();
-        return JSON.stringify(data) === JSON.stringify(EXPECTED_RESPONSE);
+        const res = await fetch(`${Environment.WaspApiUrl}/v2/node/version`);
+        return res.status === 200;
     } catch {
         return false;
     }
