2adf2e7b261e8dc6d7602e442fb24b4d4256f504
marc2332 <mespinsanz@gmail.com>
2022-12-16 17:22:23 +0100

    feat: Show info about the selected contract

---
+++ src/app/routes/Contract.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { ServiceFactory } from "../../factories/serviceFactory";
+import "./Contract.scss";
+import { ContractInfoResponse } from "../../services/wasp_client";
+import { WaspClientService } from "../../services/waspClientService";
+
+/**
+ * Contract panel.
+ * @returns The node to render.
+ */
+function Contract() {
+    const [contractInfo, setContractInfo] = useState<ContractInfoResponse | null>(null);
+    const { contractHName, chainID } = useParams();
+
+    React.useEffect(() => {
+        if (!contractHName || !chainID) {
+            return;
+        }
+
+        const waspClientService = ServiceFactory.get<WaspClientService>(WaspClientService.ServiceName);
+
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waspClientService
+            .chains()
+            .getContracts({ chainID })
+            .then((contracts) => {
+                const contract = contracts.find((c) => c.hName?.toString() === contractHName);
+                if (contract) {
+                    setContractInfo(contract);
+                }
+            });
+    }, []);
+
+    return (
+        <div className="contract">
+            <div className="contract-wrapper">
+                <h2>Contract {contractInfo?.name}</h2>
+                <div className="content">
+                    <div className="card col fill">
+                        <div className="contract-summary">
+                            <h4>INFO</h4>
+                            {contractInfo &&
+                                Object.entries(contractInfo).map(([key, val]) => (
+                                    <div key={key} className="card-item">
+                                        <span>{key}:</span>
+                                        <p className="value">{val.toString()}</p>
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contract;
