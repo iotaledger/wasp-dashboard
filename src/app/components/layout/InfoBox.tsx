8bafef1ccb03ec53e54a5b24c3155522420dd325
evavirseda <evirseda@boxfish.studio>
2022-12-20 13:55:37 +0100

    add more props

--- src/app/components/layout/InfoBox.tsx
+++ src/app/components/layout/InfoBox.tsx
@@ -5,6 +5,9 @@ interface InfoBoxProps {
     children: React.ReactNode;
     classNameCategory?: string;
     classNameTitle?: string;
+    classNameCard?: string;
+    titleWithIcon?: boolean;
+    icon?: React.ReactNode;
 }
 
 /**
@@ -14,15 +17,36 @@ interface InfoBoxProps {
  * @param children
  * @param classNameCategory
  * @param classNameTitle
+ * @param classNameCard
+ * @param titleWithIcon
+ * @param icon
  * @param title.children
  * @param title.classNameCategory
  * @param title.classNameTitle
+ * @param title.classNameCard
+ * @param title.titleWithIcon
+ * @param title.icon
  */
-function InfoBox({ title, children, classNameCategory, classNameTitle }: InfoBoxProps) {
+function InfoBox({
+    title,
+    children,
+    classNameCategory,
+    classNameTitle,
+    classNameCard,
+    titleWithIcon,
+    icon,
+}: InfoBoxProps) {
     return (
-        <div className="card col fill">
+        <div className={classNameCard ? `${classNameCard} card col fill` : "card col fill"}>
             <div className={classNameCategory ? `${classNameCategory}-summary` : ""}>
-                <h4 className={classNameTitle ?? ""}>{title}</h4>
+                {titleWithIcon ? (
+                    <div className="row middle spread margin-b-m">
+                        <h4 className={classNameTitle ?? ""}>{title}</h4>
+                        {titleWithIcon && icon}
+                    </div>
+                ) : (
+                    <h4 className={classNameTitle ?? ""}>{title}</h4>
+                )}
                 {children}
             </div>
         </div>
@@ -30,7 +54,10 @@ function InfoBox({ title, children, classNameCategory, classNameTitle }: InfoBox
 }
 
 InfoBox.defaultProps = {
+    classNameCard: "",
     classNameCategory: "",
     classNameTitle: "",
+    icon: null,
+    titleWithIcon: false,
 };
 export default InfoBox;
