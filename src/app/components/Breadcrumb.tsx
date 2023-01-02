d13e5c84a9734d22e993e5d692aa82ca48a96228
evavirseda <evirseda@boxfish.studio>
2023-01-02 13:08:06 +0100

    Add breadcrumb component and remove unused gobackbutton

---
+++ src/app/components/Breadcrumb.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ReactComponent as ChevronRight } from "../../assets/chevron-right.svg";
+interface BreadcrumbProps {
+    breadcrumbs: Breadcrumb[];
+}
+interface Breadcrumb {
+    goTo: string;
+    text: string;
+}
+/**
+ *
+ * @param breadcrumbs The breadcrumbs to render.
+ * @returns The node to render.
+ */
+
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumbs }) => {
+    const location = useLocation();
+    console.log("pathname", location);
+    return (
+        <div className="row middle">
+            {breadcrumbs.map((breadcrumb, index) => (
+                <Link
+                    className={`${
+                        location.pathname === breadcrumb.goTo ? "active-path" : ""
+                    } nav-link margin-r-t margin-b-m`}
+                    to={breadcrumb.goTo}
+                    key={index}
+                >
+                    <span className={index === breadcrumbs.length - 1 ? "last-item" : ""}>{breadcrumb.text}</span>
+                    {index < breadcrumbs.length - 1 && <ChevronRight />}
+                </Link>
+            ))}
+        </div>
+    );
+};
+export default Breadcrumb;
