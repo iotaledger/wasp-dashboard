c43f10c163f0cbbcc74e0e0b7a5bb391b535711d
evavirseda <evirseda@boxfish.studio>
2023-01-17 16:37:40 +0100

    feat: show events in selected block (#223)

--- src/app/routes/Block.tsx
+++ src/app/routes/Block.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/no-multi-comp */
 import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { ServiceFactory, WaspClientService, BlockInfoResponse, RequestReceiptResponse } from "../../lib/classes";
+import {
+    ServiceFactory,
+    WaspClientService,
+    BlockInfoResponse,
+    RequestReceiptResponse,
+    EventsResponse,
+} from "../../lib/classes";
 import "./Block.scss";
-import { KeyValueRow, Breadcrumb } from "../components";
+import { KeyValueRow, Breadcrumb, Tile } from "../components";
 
 /**
  * Block panel.
@@ -13,6 +19,7 @@ function Block() {
     const [blockInfo, setBlockInfo] = useState<BlockInfoResponse | null>(null);
     const [blockRequests, setBlockRequests] = useState<RequestReceiptResponse[]>([]);
     const [latestBlock, setLatestBlock] = useState<number>();
+    const [blockEvents, setBlockEvents] = useState<EventsResponse | null>(null);
     const { chainID, blockID } = useParams();
     const blockIndex = Number(blockID);
     const blockBreadcrumbs = [
@@ -54,8 +61,15 @@ function Block() {
                     setLatestBlock(newLatestBlock.blockIndex);
                 }
             });
-    }, [blockID]);
 
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waspClientService
+            .corecontracts()
+            .blocklogGetEventsOfBlock({ chainID, blockIndex })
+            .then(events => {
+                setBlockEvents(events);
+            });
+    }, [blockID]);
     const info = blockInfo?.gasFeeCharged ? Object.entries(blockInfo).filter(([k]) => BLOCK_DATA_VALUES.has(k)) : null;
 
     const previousBlock = (() => {
@@ -92,7 +106,7 @@ function Block() {
                     </div>
                 </div>
                 <div className="middle row">
-                    <h2 className="margin-l-s">Requests</h2>
+                    <h2>Requests</h2>
                 </div>
                 <div className="content">
                     {blockRequests.map((receipt, index) => {
@@ -142,23 +156,37 @@ function Block() {
                             </div>
                         );
                     })}
+                </div>
+                <div className="middle row">
+                    <h2>Events</h2>
+                </div>
+                <div className="content">
                     <div className="card fill">
-                        <div className="block-summary row spread-centered">
-                            <BlockLink chainID={chainID} disabled={blockIndex === 1} blockIndex={1} label="⏮️ First" />
-                            <BlockLink
-                                chainID={chainID}
-                                disabled={!previousBlock}
-                                blockIndex={previousBlock}
-                                label="⬅️ Previous"
-                            />
-                            <BlockLink chainID={chainID} disabled={!nextBlock} blockIndex={nextBlock} label="Next ➡️" />
-                            <BlockLink
-                                chainID={chainID}
-                                disabled={latestBlock === blockIndex}
-                                blockIndex={latestBlock}
-                                label="Latest ⏭️"
-                            />
+                        <div className="block-summary">
+                            {blockEvents?.events?.length === 0 ? (
+                                <Tile primaryText="No events found." />
+                            ) : (
+                                blockEvents?.events?.map(event => <Tile key={event} primaryText={event} />)
+                            )}
                         </div>
+                    </div>
+                </div>
+                <div className="card fill">
+                    <div className="block-summary row spread-centered">
+                        <BlockLink chainID={chainID} disabled={blockIndex === 1} blockIndex={1} label="⏮️ First" />
+                        <BlockLink
+                            chainID={chainID}
+                            disabled={!previousBlock}
+                            blockIndex={previousBlock}
+                            label="⬅️ Previous"
+                        />
+                        <BlockLink chainID={chainID} disabled={!nextBlock} blockIndex={nextBlock} label="Next ➡️" />
+                        <BlockLink
+                            chainID={chainID}
+                            disabled={latestBlock === blockIndex}
+                            blockIndex={latestBlock}
+                            label="Latest ⏭️"
+                        />
                     </div>
                 </div>
             </div>
