13f6e93c781ed36f8a45b9bcb1083fc90a1586d3
evavirseda <evirseda@boxfish.studio>
2023-01-23 18:09:11 +0100

    convert to functional component Home and add chains and peers

--- src/app/routes/Home.tsx
+++ src/app/routes/Home.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-import React, { ReactNode } from "react";
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import React, { useEffect, useState } from "react";
 import { EyeClosedIcon, EyeIcon } from "../../assets";
 import { ReactComponent as BannerCurve } from "../../assets/banner-curve.svg";
 import {
@@ -11,230 +11,143 @@ import {
     SettingsService,
     ThemeService,
     BrandHelper,
-} from "../../lib/classes";
-import { PeersList, AsyncComponent, InfoBox } from "../components";
+    PeeringNodeStatusResponse,
+    WaspClientService,
+    ChainInfoResponse,
+} from "../../lib/";
+import { PeersList, InfoBox, Tile } from "../components";
 import "./Route.scss";
 import "./Home.scss";
-import { HomeState } from "./HomeState";
 
 /**
  * Home panel.
+ * @returns The node to render.
  */
-class Home extends AsyncComponent<unknown, HomeState> {
-    /**
-     * The theme service.
-     */
-    private readonly _themeService: ThemeService;
+function Home() {
+    const [bannerSrc, setBannerSrc] = useState<undefined | string>();
+    const [blindMode, setBlindMode] = useState(false);
+    const [publicKey, setPublicKey] = useState<undefined | string>();
+    const [version, setVersion] = useState<undefined | string>();
+    const [networkId, setNetworkId] = useState<undefined | string>();
+    const [peersList, setPeersList] = useState<PeeringNodeStatusResponse[]>([]);
+    const [chains, setChains] = useState<ChainInfoResponse[] | null>(null);
 
-    /**
-     * The settings service.
-     */
-    private readonly _settingsService: SettingsService;
+    const authService = ServiceFactory.get<AuthService>(AuthService.ServiceName);
+    const themeService = ServiceFactory.get<ThemeService>(ThemeService.ServiceName);
+    const settingsService = ServiceFactory.get<SettingsService>(SettingsService.ServiceName);
+    const nodeConfigService = ServiceFactory.get<NodeConfigService>(NodeConfigService.ServiceName);
+    const peersService = ServiceFactory.get<PeersService>(PeersService.ServiceName);
 
-    /**
-     * The node config service.
-     */
-    private readonly _nodeConfigService: NodeConfigService;
-
-    /**
-     * The auth service.
-     */
-    private readonly _authService: AuthService;
-
-    /**
-     * The peers service.
-     */
-    private readonly _peersService: PeersService;
-
-    /**
-     * Create a new instance of Home.
-     * @param props The props.
-     */
-    constructor(props: unknown) {
-        super(props);
-
-        this._authService = ServiceFactory.get<AuthService>(AuthService.ServiceName);
-        this._themeService = ServiceFactory.get<ThemeService>(ThemeService.ServiceName);
-        this._settingsService = ServiceFactory.get<SettingsService>(SettingsService.ServiceName);
-        this._nodeConfigService = ServiceFactory.get<NodeConfigService>(NodeConfigService.ServiceName);
-        this._peersService = ServiceFactory.get<PeersService>(PeersService.ServiceName);
-
-        this.state = {
-            lastReceivedBpsTime: 0,
-            bpsIncoming: [],
-            bpsOutgoing: [],
-            bannerSrc: "",
-            blindMode: this._settingsService.getBlindMode(),
-            publicKey: "",
-            version: "",
-            networkId: "",
-            peersList: [],
+    useEffect(() => {
+        const fetchData = async () => {
+            setBannerSrc(await BrandHelper.getBanner(themeService.get()));
+            setPeersList(peersService.get());
+            if (authService.isLoggedIn()) {
+                try {
+                    await nodeConfigService.initialize();
+                    setNetworkId(nodeConfigService.getNetworkId());
+                    setVersion(nodeConfigService.getVersion());
+                    setPublicKey(nodeConfigService.getPublicKey());
+                } catch (e) {
+                    console.log(e);
+                }
+            }
         };
-    }
 
-    /**
-     * The component mounted.
-     */
-    public async componentDidMount(): Promise<void> {
-        super.componentDidMount();
+        fetchData();
 
-        this.setState({
-            bannerSrc: await BrandHelper.getBanner(this._themeService.get()),
-            peersList: this._peersService.get(),
-        });
+        const waspClientService = ServiceFactory.get<WaspClientService>(WaspClientService.ServiceName);
 
-        if (this._authService.isLoggedIn()) {
-            this._nodeConfigService
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waspClientService
+            .chains()
+            .getChains()
+            .then(newChains => {
+                setChains(newChains);
+            });
+
+        setPeersList(peersService.get());
+        setBlindMode(settingsService.getBlindMode());
+
+        if (authService.isLoggedIn()) {
+            nodeConfigService
                 .initialize()
                 .then(() => {
-                    this.setState({
-                        networkId: this._nodeConfigService.getNetworkId(),
-                        version: this._nodeConfigService.getVersion(),
-                        publicKey: this._nodeConfigService.getPublicKey(),
-                    });
+                    setNetworkId(nodeConfigService.getNetworkId());
+                    setVersion(nodeConfigService.getVersion());
+                    setPublicKey(nodeConfigService.getPublicKey());
                 })
                 .catch(e => console.log(e));
         }
 
-        EventAggregator.subscribe("theme", "home", async (theme: string) => {
-            this.setState({
-                bannerSrc: await BrandHelper.getBanner(theme),
-            });
-        });
-
-        EventAggregator.subscribe("settings.blindMode", "home", blindMode => {
-            this.setState({ blindMode });
-        });
-
-        EventAggregator.subscribe("peers-state", "home", peers => {
-            this.setState({ peersList: peers });
-        });
-    }
-
-    /**
-     * The component will unmount.
-     */
-    public componentWillUnmount(): void {
-        super.componentWillUnmount();
-        EventAggregator.unsubscribe("theme", "home");
-        EventAggregator.unsubscribe("settings.blindMode", "home");
-    }
-
-    /**
-     * Render the component.
-     * @returns The node to render.
-     */
-    public render(): ReactNode {
-        return (
-            <div className="main">
-                <div className="content">
-                    <div className="card">
-                        <div className="banner row">
-                            <div className="node-info">
-                                <div>
-                                    <h1>WASP node</h1>
-                                    <h3 className="secondary">
-                                        {this.state.blindMode ? "**********" : this.state.publicKey}
-                                    </h3>
-                                </div>
-                                <p className="secondary">{this.state.networkId}</p>
-                                <p className="secondary">{this.state.version}</p>
-                            </div>
-                            <div className="banner-image-wrapper">
-                                <div className="banner-curve">
-                                    <BannerCurve />
-                                </div>
-                                <div className="banner-image">
-                                    <img src={this.state.bannerSrc} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row fill margin-t-s desktop-down-column">
-                        <InfoBox
-                            title="Peers"
-                            titleClassName="title"
-                            titleWithIcon={true}
-                            icon={
-                                <button
-                                    type="button"
-                                    onClick={this.handleBlindMode}
-                                    className="peers-summary-blind-button"
-                                >
-                                    {this.state.blindMode ? <EyeIcon /> : <EyeClosedIcon />}
-                                </button>
-                            }
-                        >
-                            <PeersList peers={this.state.peersList} blindMode={this.state.blindMode} />
-                        </InfoBox>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    /**
-     * Check to see if a new version is available.
-     * @param currentVersion The current version.
-     * @param latestVersion The latest resion.
-     */
-    private checkVersion(currentVersion: string, latestVersion: string): void {
-        if (this.state.version !== currentVersion || this.state.latestVersion !== latestVersion) {
-            const comparison = this.compareVersions(currentVersion, latestVersion);
-
-            if (comparison < 0) {
-                this.setState({ displayLatestVersion: ` - a new version ${latestVersion} is available.` });
-            }
-        }
-    }
-
-    /**
-     * Compare two versions.
-     * @param first The first version.
-     * @param second The second versions.
-     * @returns 0 if the same, 1 if a > b or -1 if a < b.
-     */
-    private compareVersions(first: string, second: string): number {
-        const partsFirst = first.split(".");
-        const partsSecond = second.split(".");
-
-        if (partsFirst.length === 3 && partsSecond.length === 3) {
-            for (let i = 0; i < 3; i++) {
-                const na = Number.parseInt(partsFirst[i], 10);
-                const nb = Number.parseInt(partsSecond[i], 10);
-                if (na > nb) {
-                    return 1;
-                }
-                if (nb > na) {
-                    return -1;
-                }
-
-                if (i === 2) {
-                    let firstAlphabet = 96;
-                    let secondAlphabet = 96;
-                    const firstIndex = partsFirst[i].indexOf("-");
-                    if (firstIndex > 0) {
-                        firstAlphabet = partsFirst[i].codePointAt(firstIndex + 1) ?? Number.NaN;
-                    }
-                    const secondIndex = partsSecond[i].indexOf("-");
-                    if (secondIndex > 0) {
-                        secondAlphabet = partsSecond[i].codePointAt(secondIndex + 1) ?? Number.NaN;
-                    }
-
-                    return firstAlphabet - secondAlphabet;
-                }
-            }
-        }
-
-        return 0;
-    }
+        return () => {
+            EventAggregator.unsubscribe("theme", "home");
+            EventAggregator.unsubscribe("settings.blindMode", "home");
+            EventAggregator.unsubscribe("peers-state", "home");
+        };
+    }, []);
 
     /**
      * Toggle blind mode in peers list.
      */
-    private readonly handleBlindMode = (): void => {
-        this._settingsService.setBlindMode(!this.state.blindMode);
-    };
+    function handleBlindMode(): void {
+        setBlindMode(!blindMode);
+        settingsService.setBlindMode(!blindMode);
+    }
+
+    return (
+        <div className="main">
+            <div className="content">
+                <div className="card">
+                    <div className="banner row">
+                        <div className="node-info">
+                            <div>
+                                <h1>WASP node</h1>
+                                <h3 className="secondary">{blindMode ? "**********" : publicKey}</h3>
+                            </div>
+                            <p className="secondary">{networkId}</p>
+                            <p className="secondary">{version}</p>
+                        </div>
+                        <div className="banner-image-wrapper">
+                            <div className="banner-curve">
+                                <BannerCurve />
+                            </div>
+                            <div className="banner-image">
+                                <img src={bannerSrc} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row fill margin-t-s desktop-down-column">
+                    <InfoBox
+                        title="Peers"
+                        titleClassName="title"
+                        titleWithIcon={true}
+                        icon={
+                            <button type="button" onClick={handleBlindMode} className="peers-summary-blind-button">
+                                {blindMode ? <EyeIcon /> : <EyeClosedIcon />}
+                            </button>
+                        }
+                    >
+                        <PeersList peers={peersList} blindMode={blindMode} detailedList />
+                    </InfoBox>
+                </div>
+                <div className="row fill margin-t-s desktop-down-column">
+                    <InfoBox title="Chains" titleClassName="title">
+                        {chains?.map(chain => (
+                            <Tile
+                                key={chain.chainID}
+                                primaryText={chain.chainID}
+                                url={`/chains/${chain.chainID}`}
+                                displayHealth
+                                healthy={chain.isActive}
+                            />
+                        ))}
+                    </InfoBox>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
