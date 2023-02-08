e519db01ea93427619012d3efbb6959e24fe5420
Lukas Möller <lukas.moeller@iota.org>
2023-02-08 22:11:14 +0100

    Update client API

--- src/lib/wasp_client/apis/index.ts
+++ src/lib/wasp_client/apis/index.ts
@@ -1,10 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AdminApi';
+export * from './AuthApi';
 export * from './ChainsApi';
 export * from './CorecontractsApi';
 export * from './MetricsApi';
 export * from './NodeApi';
-export * from './PublicApi';
 export * from './RequestsApi';
 export * from './UsersApi';
