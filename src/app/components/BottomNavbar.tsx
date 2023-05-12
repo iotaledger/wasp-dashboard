1374fdbec49b6329a1e6276691a30bd6daea71c1
evavirseda <evirseda@boxfish.studio>
2023-05-12 15:51:58 +0200

    fix: paginator

--- src/app/components/BottomNavbar.tsx
+++ src/app/components/BottomNavbar.tsx
@@ -5,13 +5,11 @@ import { NavLinkButton } from "./Navlink";
 
 export interface BottomNavbarProps {
     navUrl: string;
-    location: string;
     firstButton: NavLinkButton;
     previousButton: NavLinkButton;
     nextButton: NavLinkButton;
     lastButton: NavLinkButton;
-    selectorOptions: string[];
-    selectorChanged: (option: string) => void;
+    pagesOptions: string[];
 }
 
 /**
@@ -21,7 +19,7 @@ export interface BottomNavbarProps {
  * @param param0.previousButton Previous button options
  * @param param0.nextButton Next button options
  * @param param0.lastButton Last button options
- * @param param0.selectorOptions Selector options
+ * @param param0.pagesOptions Selector options
  * @param param0.location Current location
  * @param param0.selectorChanged Selector changed event handler
  * @param param0.navUrl Navigation url
@@ -32,22 +30,12 @@ export default function BottomNavbar({
     previousButton,
     nextButton,
     lastButton,
-    selectorOptions,
-    location,
-    selectorChanged,
+    pagesOptions,
     navUrl,
 }: BottomNavbarProps) {
     return (
         <div className="card fill">
             <div className="summary row spread-centered middle">
-                <NavLink
-                    navUrl={navUrl}
-                    button={firstButton}
-                    label="First"
-                    icon={<ChevronLeftIcon />}
-                    codeRepetition={2}
-                    iconFirst
-                />
                 <NavLink
                     navUrl={navUrl}
                     button={previousButton}
@@ -56,35 +44,11 @@ export default function BottomNavbar({
                     iconFirst
                 />
                 <div className="row middle range-wrapper">
-                    {selectorOptions.length > 0 && (
-                        <React.Fragment>
-                            <button
-                                type="button"
-                                className="button-bottom-nav"
-                                onClick={() => selectorChanged(`${navUrl}/${selectorOptions[0]}`)}
-                            >
-                                {selectorOptions[0]}
-                            </button>
-                            <span> ... </span>
-                            <button
-                                type="button"
-                                className="button-bottom-nav"
-                                onClick={() =>
-                                    selectorChanged(`${navUrl}/${selectorOptions[selectorOptions.length - 1]}`)}
-                            >
-                                {selectorOptions[selectorOptions.length - 1]}
-                            </button>
-                        </React.Fragment>
-                    )}
+                    <NavLink navUrl={navUrl} button={firstButton} label={pagesOptions[0]} />
+                    <span>. . .</span>
+                    <NavLink navUrl={navUrl} button={lastButton} label={pagesOptions[pagesOptions.length - 1]} />
                 </div>
                 <NavLink navUrl={navUrl} button={nextButton} label="Next" icon={<ChevronRightIcon />} />
-                <NavLink
-                    navUrl={navUrl}
-                    button={lastButton}
-                    label="Latest"
-                    icon={<ChevronRightIcon />}
-                    codeRepetition={2}
-                />
             </div>
         </div>
     );
