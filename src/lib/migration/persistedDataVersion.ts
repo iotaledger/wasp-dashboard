d64445a8574fe7bd9ec7cbe034579f1a7a7bbf95
evavirseda <evirseda@boxfish.studio>
2023-05-24 11:31:39 +0200

    feat: add mapping to migrations

--- src/lib/migration/persistedDataVersion.ts
+++ src/lib/migration/persistedDataVersion.ts
@@ -3,7 +3,20 @@ import { ChainData } from "../classes/services/chainsService";
 import { MAX_CACHED_BLOCKS } from "../constants";
 import { LocalStorageKey } from "../enums";
 
+interface Migration {
+    version: number;
+    migrate: () => void;
+}
+
+const migrations: Migration[] = [
+    {
+        version: 2,
+        migrate: migrateToVersion2,
+    },
+];
+
 const CURRENT_PERSISTED_DATA_VERSION = 2;
+
 /**
  *
  */
@@ -22,31 +35,22 @@ function migrateEachVersion(): void {
     const storageService = ServiceFactory.get<LocalStorageService>(LocalStorageService.ServiceName);
     let version: number = storageService.load(LocalStorageKey.PersistedDataVersion) ?? 1; // Get the current version
     for (CURRENT_PERSISTED_DATA_VERSION; version < CURRENT_PERSISTED_DATA_VERSION; version++) {
-        migratePersistedChains(version + 1);
+        migrations.find(migration => migration.version === version)?.migrate();
         storageService.save(LocalStorageKey.PersistedDataVersion, version + 1);
     }
 }
 
 /**
  *
- * @param migrationVersion
  */
-function migratePersistedChains(migrationVersion: number): void {
+function migrateToVersion2(): void {
     const storageService = ServiceFactory.get<LocalStorageService>(LocalStorageService.ServiceName);
     const chains: Record<string, ChainData> = storageService.load(LocalStorageKey.Chains) ?? {};
-    switch (migrationVersion) {
-        case 2: {
-            if (chains && Object.keys(chains).length > 0) {
-                const chainID = Object.keys(chains)[0];
-                const blocks = [...chains[chainID].blocks].filter(block => block !== undefined && block !== null);
-                chains[chainID].blocks =
-                    blocks.length > MAX_CACHED_BLOCKS ? blocks.splice(0, MAX_CACHED_BLOCKS) : blocks;
-            }
-            break;
-        }
-        default: {
-            break;
-        }
+
+    if (chains && Object.keys(chains).length > 0) {
+        const chainID = Object.keys(chains)[0];
+        const blocks = [...chains[chainID].blocks].filter(block => block !== undefined && block !== null);
+        chains[chainID].blocks = blocks.length > MAX_CACHED_BLOCKS ? blocks.splice(0, MAX_CACHED_BLOCKS) : blocks;
     }
     storageService.save(LocalStorageKey.Chains, chains);
 }
